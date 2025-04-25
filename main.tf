00e04dcb05cf9b8d9e16989c6c617ad941611bdd
Rafaf Tahsin <rafaf@sheba.xyz>
2025-04-25 22:50:25 +0600

    Initial

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.96.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-southeast-1"
+
+}
+
