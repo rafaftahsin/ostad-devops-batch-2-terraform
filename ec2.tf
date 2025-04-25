1ecacc8270b5c1e47f733620f1819c498925eb05
Rafaf Tahsin <rafaf@sheba.xyz>
2025-04-25 23:40:52 +0600

    updated ec2

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "app_server" {
-  ami           = "ami-0c1907b6d738188e5"
+  ami           = "ami-00695597b5e6018d3"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-e17dff87" ]
 
   tags = {
     Name = "ExampleAppServerInstance"
