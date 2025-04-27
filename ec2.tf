c7cae0372aa5384575d5d86d5255821e1069a140
Rafaf Tahsin <rafaf@sheba.xyz>
2025-04-27 23:43:50 +0600

    updated

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,42 @@
 resource "aws_instance" "app_server" {
   ami           = "ami-00695597b5e6018d3"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [ "sg-e17dff87" ]
+  vpc_security_group_ids = [ aws_security_group.allow_all.id ]
 
   tags = {
     Name = "ExampleAppServerInstance"
   }
 }
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 3000
+    to_port          = 3000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+    ingress {
+    from_port        = 8000
+    to_port          = 8000
+    protocol         = "tcp"
+    ipv6_cidr_blocks = ["::/0"]
+
+  }
+
+  tags = {
+    Name = "allow_all"
+  }
+}
+
--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "ec2-instance-public-ip" {
   value = aws_instance.app_server.public_ip
 }
+
+output "security-group-id" {
+  value = aws_security_group.allow_all.id
+}
